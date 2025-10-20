b291f9e3b43531beba87763bb2744b292c00e79f
Sifat Ullah <sifatullahhere@gmail.com>
2025-10-20 07:10:35 +0600

    Update uiu_smart_campus.sql

--- uiu_smart_campus.sql
+++ uiu_smart_campus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2025 at 02:45 AM
+-- Generation Time: Oct 20, 2025 at 03:07 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
